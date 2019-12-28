afb9edd652b5a44f8f38cfd9ee61b4259ed2b294
Phil Borman <phil.borman@gmail.com>
2019-12-28 18:22:19 +0100

    Additional debugging for windows update failure

--- lazylibrarian/versioncheck.py
+++ lazylibrarian/versioncheck.py
@@ -560,14 +560,15 @@ def update():
             return False
         content_dir = os.path.join(update_dir, update_dir_contents[0])
         logger.debug("update_dir_contents [%s]" % str(update_dir_contents))
-        logger.debug ("Walking %s" % content_dir)
+        logger.debug("Walking %s" % content_dir)
         # walk temp folder and move files to main folder
         for rootdir, _, filenames in walk(content_dir):
-            logger.debug("PROG_DIR [%s] content_dir [%s] rootdir [%s]" %
-                (lazylibrarian.PROG_DIR, content_dir, rootdir))
             for curfile in filenames:
                 old_path = os.path.join(content_dir, rootdir, curfile)
                 new_path = os.path.join(lazylibrarian.PROG_DIR, rootdir, curfile)
+                if old_path == new_path:
+                    logger.error("PROG_DIR [%s] content_dir [%s] rootdir [%s] curfile [%s]" % (
+                        lazylibrarian.PROG_DIR, content_dir, rootdir, curfile))
                 if os.path.isfile(new_path):
                     os.remove(new_path)
                 os.renames(old_path, new_path)
