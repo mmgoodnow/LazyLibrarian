685077706cb14a4e92ed08581b45bc70074ee600
Phil Borman <phil.borman@gmail.com>
2020-07-05 20:16:31 +0200

    Add missing "coding" line

--- lazylibrarian/formatter.py
+++ lazylibrarian/formatter.py
@@ -1,4 +1,5 @@
 #  This file is part of Lazylibrarian.
+# coding: utf-8
 #  Lazylibrarian is free software':'you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
