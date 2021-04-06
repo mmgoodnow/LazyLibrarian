9e0652804a7c0cfa796c3f261eea407ab3d8e044
cbartondock <cbartondock@gmail.com>
2021-04-06 13:31:01 -0400

    fix division by zero error

--- lazylibrarian/common.py
+++ lazylibrarian/common.py
@@ -893,7 +893,7 @@ def scheduleJob(action='Start', target=None):
                 logger.debug("Found %s %s from %s %s update" % (
                              overdue, plural(overdue, typ), total, due))
                 interval = maxage * 60 * 24
-                interval = interval / total
+                interval = interval / max(total,1)
                 interval = int(interval * 0.9)  # allow some update time
 
                 if interval < 5:  # set a minimum interval of 5 minutes so we don't upset goodreads/librarything api
