dc83e0d3104ab30343367ede547ea2a63be54da7
Phil Borman <phil.borman@gmail.com>
2021-04-07 15:15:21 +0200

    indent tidying

--- example_rclone_link.sh
+++ example_rclone_link.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then 
+if [ "$#" -ne 1 ]; then
   	echo "USAGE: link = rclonelink.sh /path/to/filename"
 else
     A=$1
     R="Dropbox"
-	F=`echo $A | rev | cut -d '/' -f 1 | rev`
-	rclone copy "$A" $R:
-	X=`rclone link --expire 14d $R:"$F"`
-	echo "$X  The link is valid for 14 days"
+    F=`echo $A | rev | cut -d '/' -f 1 | rev`
+    rclone copy "$A" $R:
+    X=`rclone link --expire 14d $R:"$F"`
+    echo "$X  The link is valid for 14 days"
 fi
