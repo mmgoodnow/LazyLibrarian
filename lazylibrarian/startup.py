43f2b328b6d0f927ba7137a74b1e0486839b24ec
phil.borman@gmail.com <phil.borman@gmail.com>
2025-10-12 09:57:50 +0200

    Store which providers offer subtitles

--- lazylibrarian/startup.py
+++ lazylibrarian/startup.py
@@ -326,15 +326,15 @@ class StartupLazyLibrarian:
     def build_sources():
         info_sources = {
                 'OpenLibrary': {'src': 'OL', 'author_key': 'ol_id', 'book_key': 'ol_id', 'enabled': 'OL_API',
-                                'api': ol.OpenLibrary()},
+                                'api': ol.OpenLibrary(), 'has_subs': 0},
                 'GoodReads': {'src': 'GR', 'author_key': 'gr_id', 'book_key': 'gr_id', 'enabled': 'GR_API',
-                              'api': gr.GoodReads()},
+                              'api': gr.GoodReads(), 'has_subs': 0},
                 'HardCover': {'src': 'HC', 'author_key': 'hc_id', 'book_key': 'hc_id', 'enabled': 'HC_API',
-                              'api': hc.HardCover()},
+                              'api': hc.HardCover(), 'has_subs': 1},
                 'GoogleBooks': {'src': 'GB', 'author_key': 'authorid', 'book_key': 'gb_id', 'enabled': 'GB_API',
-                                'api': gb.GoogleBooks()},
+                                'api': gb.GoogleBooks(), 'has_subs': 1},
                 'DNB': {'src': 'DN', 'author_key': 'authorid', 'book_key': 'dnb_id', 'enabled': 'DNB_API',
-                        'api': dnb.DNB()},
+                        'api': dnb.DNB(), 'has_subs': 1},
                 }
         adminlogger = logging.getLogger('special.admin')
         adminlogger.debug(info_sources)
